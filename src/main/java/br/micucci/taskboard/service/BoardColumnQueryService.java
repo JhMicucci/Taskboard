afb7272c872a9c87ac998817cf676b6f677c9bbb
João Miccuci <joaomicucci3@gmail.com>
2025-04-06 18:56:15 -0300

    autualizacao no dao e services

---
+++ src/main/java/br/micucci/taskboard/service/BoardColumnQueryService.java
@@ -0,0 +1,21 @@
+package br.micucci.taskboard.service;
+
+import br.micucci.taskboard.persistence.dao.BoardColumnDAO;
+import br.micucci.taskboard.persistence.entity.BoardColumnEntity;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class BoardColumnQueryService {
+
+    private final Connection connection;
+
+    public Optional<BoardColumnEntity> findById(final Long id) throws SQLException {
+        var dao = new BoardColumnDAO(connection);
+        return dao.findById(id);
+    }
+
+}

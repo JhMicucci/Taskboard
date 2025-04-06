2053b8e7977b870706ba6e5c45695f334acdb9c7
João Miccuci <joaomicucci3@gmail.com>
2025-04-06 18:17:13 -0300

    desenvolvendo o menu

---
+++ src/main/java/br/micucci/taskboard/ui/BoardMenu.java
@@ -0,0 +1,13 @@
+package br.micucci.taskboard.ui;
+
+
+import br.micucci.taskboard.persistence.entity.BoardEntity;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class BoardMenu {
+
+    private final BoardEntity entity;
+
+    public void execute(){}
+}
